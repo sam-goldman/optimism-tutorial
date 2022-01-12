1d59e0ae96af178e568eafc9fcb500dff0d92abd
Ori Pomerantz <qbzzt1@gmail.com>
2022-01-11 21:22:03 -0600

    A few minor changes

--- gas-estimate/scripts/gas-estimate.js
+++ gas-estimate/scripts/gas-estimate.js
@@ -1,19 +1,7 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
 const optimismContracts = require("@eth-optimism/contracts")
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
-
   // Get network information
   await hre.ethers.provider._networkPromise
   const provider = hre.ethers.provider
